dfda9b12051b5ea2ebe10036b8e3d5c8febccf00
leorpedroso <leorpedroso97@gmail.com>
2021-10-14 19:25:46 -0300

    q2 draft now passes the autograder (with -q q2)

--- src/analysis.py
+++ src/analysis.py
@@ -21,7 +21,8 @@
 
 def question2():
     answerDiscount = 0.9
-    answerNoise = 0.2
+    #answerNoise = 0.60711209
+    answerNoise = 0.015
     return answerDiscount, answerNoise
 
 def question3a():
